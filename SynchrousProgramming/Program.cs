3bc02b3b58eb7680d30b76e478849e1a59c58167
tooprajaw <tooprajaw@gmail.com>
2023-02-14 11:36:29 +0700

    Add files via upload

---
+++ SynchrousProgramming/Program.cs
@@ -0,0 +1,87 @@
+﻿using Cores;
+
+namespace SynchrousProgramming
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PourCoffee();
+            Console.WriteLine("coffee is ready");
+
+            FryEggs(2);
+            Console.WriteLine("eggs are ready");
+
+            FryBacon(3);
+            Console.WriteLine("bacon is ready");
+
+            Toast toast = ToastBread(2);
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+            Console.WriteLine("Breakfast is ready!");
+        }
+
+        private static Juice PourOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        private static void ApplyJam(Toast toast)
+        {
+            // Apply jam to toast
+            Console.WriteLine("Putting jam on the toast");
+        }
+
+        private static void ApplyButter(Toast toast)
+        {
+            // Apply butter to toast
+            Console.WriteLine("Putting butter on the toast");
+        }
+
+        private static Toast ToastBread(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+            Console.WriteLine("Start toasting...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Remove toast from toaster");
+            return new Toast();
+        }
+
+        private static void FryBacon(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+            Console.WriteLine("cooking first side of bacon...");
+            Task.Delay(3000).Wait();
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+            Console.WriteLine("cooking the second side of bacon...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put bacon on plate");
+        }
+
+        private static void FryEggs(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put eggs on plate");
+        }
+
+        private static void PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+        }
+    }
+}
